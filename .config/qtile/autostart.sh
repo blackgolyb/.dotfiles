a873b03b2b56c92b1aead6286b4bbc350a80e281
AndrewOmelnitsky <andrejomelnickij@gmail.com>
2023-02-16 19:50:06 +0200

    update

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 glava &
+dunst &
--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -12,9 +12,17 @@ from libqtile.config import Click, Drag, Group, Key, EzKey, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
+from qtile_extras import widget as qe_widget
+
+
+import locale
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+
 
 home_path = os.path.expanduser('~')
 config_path = home_path + '/.config/qtile/'
+text_editor = 'code'
+webbrowser = 'firefox'
 
 group_gaps = 7
 around_gaps = 7
@@ -22,31 +30,30 @@ bar_gaps = [0, 0, 7, 0]
 rounded_bar = False
 
 
-
 # АВТОЗАПУСК ----------------------------------------------------------------------
 @hook.subscribe.startup_once
 def autostart():
     subprocess.call([config_path + 'autostart.sh'])
-    
-    
-def is_proces_run(process_name):    
+
+
+def is_proces_run(process_name):
     for proc in psutil.process_iter():
         if proc.name() == process_name:
             return True
-        
+
     return False
-    
+
 
 @hook.subscribe.startup
 def _startup():
     global mybar, rounded_bar
     mybar.window.window.set_property("QTILE_BAR", 1, "CARDINAL", 32)
-    
+
     addition_process = ''
     if is_proces_run('picom'):
         sleep_time = 0.5
         addition_process = f'pkill picom; sleep {sleep_time}; '
-        
+
     if rounded_bar:
         subprocess.run(addition_process + 'picom -b', shell=True)
     else:
@@ -56,6 +63,8 @@ def _startup():
         )
 
 # СДЕЛАТЬ ДИАЛОГОВЫЕ ОКНА ПЛАВАЮЩИМИ ----------------------------------------------
+
+
 @hook.subscribe.client_new
 def floating_dialogs(window):
     dialog = window.window.get_wm_type() == 'dialog'
@@ -66,6 +75,7 @@ def floating_dialogs(window):
 
 class StickyManagement:
     """Класс который реализует возможность закреплять окна на рабочих поверхностях"""
+
     def __init__(self):
         self.window_list = list()
 
@@ -85,29 +95,30 @@ class StickyManagement:
 
     def pin_window(self, qtile):
         current_window = qtile.current_window
-        
+
         if current_window not in self.window_list:
             self._pin_window(current_window)
-            
+
     def unpin_window(self, qtile):
         current_window = qtile.current_window
-        
+
         if current_window in self.window_list:
             self._unpin_window(current_window)
-            
+
     def init_hooks(self):
         window_list = self.window_list
-        
+
         @hook.subscribe.setgroup
         def _move_sticky_window_to_current_group():
             for window in window_list:
                 window.togroup(qtile.current_group.name)
                 window.cmd_bring_to_front()
-                
+
         @hook.subscribe.client_managed
         def _display_pined_window_above_other(window_):
             for window in window_list:
                 window.cmd_bring_to_front()
+
 
 sticky_management = StickyManagement()
 sticky_management.init_hooks()
@@ -157,7 +168,7 @@ keys = [
     # Запуск приложений
     Key([mod], "Return", lazy.spawn("alacritty")),
     # Key([mod], "f", lazy.spawn("firefox --wayland")), # Это для вайланда
-    Key([mod], "f", lazy.spawn("firefox")),  # Для иксов
+    Key([mod], "f", lazy.spawn(webbrowser)),  # Для иксов
     Key([mod], "e", lazy.spawn("dolphin")),
     Key([mod], "t", lazy.spawn("telegram-desktop")),
     Key([mod], "space", lazy.spawn("rofi -show drun")),
@@ -178,17 +189,19 @@ keys = [
     Key([mod], "d", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 
     # Раскладка клавиатуры
-    #Key([mod], "space", lazy.widget["keyboardlayout"].next_keyboard(), desc="Next keyboard layout."),
-    Key([mod], "Shift_L", lazy.widget["keyboardlayout"].next_keyboard(), desc="Next keyboard layout."),
-    
-    
-    
+    # Key([mod], "space", lazy.widget["keyboardlayout"].next_keyboard(), desc="Next keyboard layout."),
+    Key([mod], "Shift_L", lazy.widget["keyboardlayout"].next_keyboard(),
+        desc="Next keyboard layout."),
+
+
+
     # Штука которая позволяет закрепить окно на всех рабочих поверхностях
     # Т.е. окно будет следовать за вами на всех робочих столах
-    Key([mod], "o", lazy.function(sticky_management.toggle_sticky_window), desc="toggle stick win"),
+    Key([mod], "o", lazy.function(
+        sticky_management.toggle_sticky_window), desc="toggle stick win"),
 
-    
-    
+
+
 
     # Яхз что за это, типо все окна на месте одного окна отображаются.
     # Toggle between split and unsplit sides of stack.
@@ -206,19 +219,24 @@ keys = [
     # Нужно установить gnome-screenshot
     Key([mod], "Print", lazy.spawn('gnome-screenshot -i')),
 
-    
+
     # Контроль звука и яркости
-    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer set Master 5%- unmute")),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer set Master 5%+ unmute")),
-    Key([], "XF86AudioMute", lazy.spawn("amixer set Master togglemute")),
+    Key([], "XF86AudioLowerVolume", lazy.spawn(
+        "bash " + config_path + "change_volume down")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn(
+        "bash " + config_path + "change_volume up")),
+    Key([], "XF86AudioMute", lazy.spawn(
+        "bash " + config_path + "change_volume mute")),
     Key([], "XF86AudioMicMute", lazy.spawn("amixer set Capture togglemute")),
 
 
     # Яркость нужно установить light
     # sudo chmod +s /usr/bin/light для работы утилиты light
-    Key([], "XF86MonBrightnessDown", lazy.spawn("light -U 5")),
-    Key([], "XF86MonBrightnessUp", lazy.spawn("light -A 5")),
-    
+    Key([], "XF86MonBrightnessDown", lazy.spawn(
+        "bash " + config_path + "change_brightness down")),
+    Key([], "XF86MonBrightnessUp", lazy.spawn(
+        "bash " + config_path + "change_brightness up")),
+
 ]
 
 
@@ -236,6 +254,7 @@ get_superscript_by_normal = {
     '9': '⁹',
     'w': 'ʷ',
 }
+
 
 class GroupCreator:
     is_subscript = False
@@ -307,9 +326,23 @@ layouts = [
     # layout.Columns(),
     # layout.Max(), # Фуллскрин
     # layout.Stack(num_stacks=2), #Какая то фигня
-    layout.Bsp(border_focus="#C3C3C3", border_normal="#2E3440",
-               border_width=1,
-               margin=[0, 0, group_gaps, group_gaps]),  # Как в bspwm
+    layout.Bsp(
+        border_focus="#C3C3C3",
+        border_normal="#2E3440",
+        border_width=1,
+        # lower_right=False,
+        # ratio=-1.6,
+        # fair=False,
+        margin=[0, 0, group_gaps, group_gaps]
+    ),  # Как в bspwm
+
+    layout.MonadTall(
+        border_focus="#C3C3C3",
+        border_normal="#2E3440",
+        border_width=1,
+        margin=[0, 0, group_gaps, group_gaps]
+    ),  # Как в bspwm
+
     # layout.Matrix(), # В 2 колонки
     # layout.MonadTall(), # Как в dwm
     # layout.MonadWide(), # Как в dwm только по горизонтали
@@ -330,19 +363,19 @@ widget_defaults = dict(
 extension_defaults = widget_defaults.copy()
 
 
-
 class MyBatteryIcon(widget.BatteryIcon):
     offsety = -2
     # offsetx = -0
 
 
-
 rofi_wifi_menu = f'bash {home_path}/.config/rofi-network-manager/rofi-network-manager.sh'
+rofi_bluetooth_menu = f'bash {home_path}/.config/rofi-bluetooth/rofi-bluetooth'
+
 
 class MyBattery(widget.Battery):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
         def get_prep__get_param(func):
             def _get_param(name):
                 try:
@@ -350,20 +383,80 @@ class MyBattery(widget.Battery):
                 except:
                     return None
             return _get_param
-        
-        self._battery._get_param = get_prep__get_param(self._battery._get_param)
-        
+
+        self._battery._get_param = get_prep__get_param(
+            self._battery._get_param)
+
 
 # /usr/share/icons/breeze-dark/status
 # ВИДЖЕТЫ НА ПАНЕЛИ И ИХ ПАРАМЕТРЫ ------------------------------------------------
+decor = {
+    "decorations": [
+        qe_widget.decorations.BorderDecoration(
+            colour="#ffffff",
+            border_width=1,
+        )
+    ],
+    # "padding": 18,
+}
+
 bar_widgets = [
+    # Menu
+    widget.WidgetBox(
+        text_closed='  ',
+        text_open='  ',
+        padding=3,
+        widgets=[
+            widget.TextBox(text="|"),
+            widget.Spacer(length=5),
+
+            # Запуск bpython
+            widget.TextBox(
+                text=" ",
+                mouse_callbacks={'Button1': lazy.spawn(
+                    "alacritty -e bpython")},
+            ),
+            widget.Spacer(length=5),
+
+            # Открытие конфига qtile в редакторе кода
+            widget.TextBox(
+                text=" ",
+                mouse_callbacks={'Button1': lazy.spawn(
+                    f"{text_editor} {config_path + 'config.py'}")},
+            ),
+            widget.Spacer(length=5),
+
+
+            qe_widget.TextBox(
+                text="SEMPAI",
+                margin=6,
+                padding=3,
+                mouse_callbacks={'Button1': lazy.spawn(
+                    f"{webbrowser} https://anifap.me/")},
+                **decor
+            ),
+            widget.Image(
+                filename=config_path+'menu_images/1.jpg',
+                mouse_callbacks={'Button1': lazy.spawn(
+                    "alacritty -e bpython")},
+            ),
+
+            widget.Spacer(length=5),
+            widget.TextBox(text="|"),
+        ]
+    ),
+
+    widget.Spacer(length=20),
+
     # widget.CurrentLayout(), # Текущий макет
     # Иконки воркспейсов
-    base_groupbox:=widget.GroupBox(
+    base_groupbox := widget.GroupBox(
         borderwidth=1,  # Толщина рамки
-        highlight_method='line',  # ('border', 'block', 'text', 'line') # Метод выделения активного воркспейса
-        block_highlight_text_color='#ffffff',  # '#DDDFE5',  # Цвет текста активного воркспейса
-        this_current_screen_border='#ffffff',  #C3C3C3  Цвет фона активного воркспейса
+        # ('border', 'block', 'text', 'line') # Метод выделения активного воркспейса
+        highlight_method='line',
+        # '#DDDFE5',  # Цвет текста активного воркспейса
+        block_highlight_text_color='#ffffff',
+        this_current_screen_border='#ffffff',  # C3C3C3  Цвет фона активного воркспейса
         inactive='#777777',
         active='#ffffff',
         other_screen_border='#3333ff',
@@ -387,13 +480,13 @@ bar_widgets = [
     ),
     widget.Spacer(),
 
-    widget.Systray(), # Трэй, не работает в вялом, нужно будет юзать widget.StatusNotifier
+    widget.Systray(),  # Трэй, не работает в вялом, нужно будет юзать widget.StatusNotifier
     widget.Spacer(length=15),
 
 
     widget.WidgetBox(
-        text_closed = '  ',
-        text_open = '  ',
+        text_closed='  ',
+        text_open='  ',
         padding=0,
         widgets=[
             widget.TextBox(text="|"),
@@ -403,13 +496,17 @@ bar_widgets = [
                 text="",
                 mouse_callbacks={'Button1': lazy.spawn(rofi_wifi_menu)},
             ),
-            widget.Wlan(format='{essid}', padding=0),
+            widget.Wlan(
+                format='{essid}',
+                padding=0,
+                mouse_callbacks={'Button1': lazy.spawn(rofi_wifi_menu)},
+            ),
             widget.Spacer(length=8),
 
 
             # Яркость
             widget.TextBox(text="󰖨"),
-            blw:=widget.Backlight(padding=0),
+            blw := widget.Backlight(padding=0),
             widget.Spacer(length=15),
 
             # Обновлений пакетов
@@ -419,8 +516,14 @@ bar_widgets = [
                 no_update_string=' 0',
                 padding=0
             ),
-
             widget.Spacer(length=6),
+
+            # Bluetooth
+            widget.TextBox(
+                text="",
+                mouse_callbacks={'Button1': lazy.spawn(rofi_bluetooth_menu)},
+            ),
+
             widget.TextBox(text="|"),
             widget.Spacer(length=8),
         ]
@@ -434,17 +537,18 @@ bar_widgets = [
         mouse_callbacks={'Button1': lazy.spawn("pavucontrol")},
         padding=7
     ),
-    widget.PulseVolume(limit_max_volume=True, padding=0),  # Виджет громкости пульсы
+    # Виджет громкости пульсы
+    widget.PulseVolume(limit_max_volume=True, padding=0),
     widget.Spacer(length=15),  # Виджет пробела
 
     # Keyboard layout
     widget.KeyboardLayout(configured_keyboards=['us', 'ru', 'ua'],
-                            update_interval=1,
-                            padding=0),
+                          update_interval=1,
+                          padding=0),
     widget.Spacer(length=15),
 
     # Battery
-    battery_widget:=widget.BatteryIcon(
+    battery_widget := widget.BatteryIcon(
         theme_path=config_path + 'battery_icons',
         padding=0,
         update_interval=5,
@@ -460,26 +564,24 @@ bar_widgets = [
 
     # Clock
     widget.WidgetBox(
-        text_closed = ' ',
-        text_open = ' ',
+        text_closed=' ',
+        text_open=' ',
         padding=0,
         widgets=[
-            # дата
-            widget.Clock(format="%d-%m-%y %h", padding=0),
+            # Дата
+            widget.Clock(format="[%d %B %Y | %A]", padding=0),
             widget.Spacer(length=5),
         ]
     ),
     widget.Spacer(length=5),
 
     widget.Clock(format="%H:%M",
-                    padding=0),  # Время и дата
+                 padding=0),  # Время и дата
     widget.Spacer(length=20),  # Виджет пробела
 
     # Quit
     widget.QuickExit(default_text='', padding=6),  # Кнопка выключения
 ]
-
-
 
 
 screens = [
@@ -489,12 +591,14 @@ screens = [
         right=bar.Gap(around_gaps),
         left=bar.Gap(around_gaps - group_gaps),
         bottom=bar.Gap(around_gaps - group_gaps),
-        top=(mybar:=bar.Bar(  # Расположение бара
+        top=(mybar := bar.Bar(  # Расположение бара
             bar_widgets,
             20,  # Высота панели
             border_width=[2, 16, 2, 10],  # Толщина рамок панели
-            border_color=["2E3440", "2E3440", "2E3440", "2E3440"],  # 777777  C3C3C3   Цвет рамок панели
+            # 777777  C3C3C3   Цвет рамок панели
+            border_color=["2E3440", "2E3440", "2E3440", "2E3440"],
             # border_color=["2E3440", "2E3440", "2E3440", "2E3440"],  # Цвет рамок панели
+            # opacity=0.95,
             margin=bar_gaps,  # Гапсы бара
             background="#2E3440"  # Цвет фона панели
             # opacity=0,5 # Прозрачность бара
@@ -521,7 +625,8 @@ screens = [
             ],
             30,
             border_width=[2, 2, 2, 2],  # Толщина рамок панели
-            border_color=["2E3440", "2E3440", "ffffff", "2E3440"],  # 777777  C3C3C3   Цвет рамок панели
+            # 777777  C3C3C3   Цвет рамок панели
+            border_color=["2E3440", "2E3440", "ffffff", "2E3440"],
             # border_color=["2E3440", "2E3440", "2E3440", "2E3440"],  # Цвет рамок панели
             margin=[10, 725, 0, 725],  # Гапсы бара
             background="#2E3440"  # Цвет фона панели
@@ -546,11 +651,13 @@ def battery_widget_draw(self) -> None:
     self.drawer.clear(self.background or self.bar.background)
     image = self.images[self.current_icon]
     self.drawer.ctx.save()
-    self.drawer.ctx.translate(0, (self.bar.height - image.height) // 2 + self.y_padding)
+    self.drawer.ctx.translate(
+        0, (self.bar.height - image.height) // 2 + self.y_padding)
     self.drawer.ctx.set_source(image.pattern)
     self.drawer.ctx.paint()
     self.drawer.ctx.restore()
-    self.drawer.draw(offsetx=self.offset, offsety=self.offsety, width=self.length)
+    self.drawer.draw(offsetx=self.offset,
+                     offsety=self.offsety, width=self.length)
 
 # res = battery_widget.setup_images
 # battery_widget.setup_images = battery_widget_configure(battery_widget, res)
@@ -560,10 +667,13 @@ def battery_widget_draw(self) -> None:
 # battery_widget.draw = lambda: battery_widget_draw(battery_widget)
 
 # Переделываем виджет яркости под утилиту light
+
+
 def get_brightness():
     result = subprocess.run(['light', '-G'], capture_output=True, text=True)
     brightness = result.stdout.replace("\n", "")
     return float(brightness)/100
+
 
 blw._get_info = get_brightness
 
