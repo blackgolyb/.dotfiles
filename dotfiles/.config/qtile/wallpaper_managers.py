06a9f737b4f375cc39de007189a0d71d22fe87ee
Blackgolyb <andrejomelnickij@gmail.com>
2024-12-11 19:41:22 +0200

    feat(qtile/wallpapers): add some more and change priority

--- dotfiles/.config/qtile/wallpaper_managers.py
+++ dotfiles/.config/qtile/wallpaper_managers.py
@@ -2,10 +2,12 @@ from settings import resources_path
 from screens import default_screens as screens
 from services.wallpaper_manager import WallpaperManager
 
+
 wallpaper_priority = [
     ("8.jpg", 7),
     ("10.png", 7),
-    ("13.jpeg", 14),
+    ("13.jpeg", 7),
+    ("14.jpg", 7),
 ]
 
 wallpaper_managers = [
