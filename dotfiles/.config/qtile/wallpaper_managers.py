40160c8140d93dd3d217e694b86c49d8dc2bb051
Blackgolyb <andrejomelnickij@gmail.com>
2025-03-15 13:07:39 +0200

    feat(qtile): add wallpaper

--- dotfiles/.config/qtile/wallpaper_managers.py
+++ dotfiles/.config/qtile/wallpaper_managers.py
@@ -1,13 +1,13 @@
-from settings import resources_path
 from screens import default_screens as screens
 from services.wallpaper_manager import WallpaperManager
-
+from settings import resources_path
 
 wallpaper_priority = [
     ("8.jpg", 7),
     ("10.png", 7),
-    ("13.jpeg", 7),
+    ("5.png", 7),
     ("14.jpg", 7),
+    ("15.png", 7),
 ]
 
 wallpaper_managers = [
