97a7a21f3b0e2ba61db191a532bbc70ce1295ef5
Blackgolyb <andrejomelnickij@gmail.com>
2025-05-21 23:31:41 +0300

    feat: update dotfiles script

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,13 +1,45 @@
 #!/usr/bin/env sh
 
-DOTFILES_DIR=".dotfiles"
-DOTFILES_PATH="$HOME/$DOTFILES_DIR"
+function update_config() {
+    PROJECT_DIR=$(get_dotfiles_dir)
+    SCRIPTS_DOTFILES_DIR=${PROJECT_DIR}/scripts
+    sh $SCRIPTS_DOTFILES_DIR/install_config.sh
+}
+
+function go_to_dotfiles() {
+    dotdir=$(get_dotfiles_dir)
+    cd $dotdir
+}
+
+function open_dotfiles() {
+    go_to_dotfiles
+    if [[ -n "${EDITOR}" ]]; then
+        $($EDITOR .)
+    fi
+}
+
+function sync_dotfiles() {
+    go_to_dotfiles && \
+    git pull --rebase && \
+    git push && \
+    update_config
+}
 
 case $1 in
-    "update")
+    "sync")
+        sync_dotfiles
+        ;;
+    "update_config")
+        update_config
+        ;;
+    "go")
+        go_to_dotfiles
+        ;;
+    "open")
+        open_dotfiles
         ;;
     "")
-        echo "You should provide option"
+        open_dotfiles
         ;;
     *)
         echo "No option $1"
