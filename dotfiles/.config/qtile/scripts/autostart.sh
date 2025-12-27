29fa50c8b4bc3ff955e31dd551ed366f956a2699
Blackgolyb <andrejomelnickij@gmail.com>
2025-12-27 15:21:02 +0200

    feat(qtile): update autostart

--- dotfiles/.config/qtile/scripts/autostart.sh
+++ dotfiles/.config/qtile/scripts/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # gamma correction for ThinkPad t480 yellow screen
 xgamma -rgamma 0.95 -ggamma 0.95 -bgamma 1.1 &
+xset b off &
 
 # glava &
 # eww open --config ~/.config/eww/test bar
@@ -9,7 +10,7 @@ greenclip daemon &
 tmux start &
 
 zen-browser &
-telegram-desktop &
+superproductivity &
 
 # setxkbmap -option ctrl:swapcaps
 # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
