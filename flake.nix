0ee508441403b6be77b1abf0533d2742064eef30
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-05 00:20:24 +0200

    feat(flake): update

--- flake.nix
+++ flake.nix
@@ -7,20 +7,36 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
-    zen-browser.inputs.home-manager.follows = "home-manager";
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = {self, nixpkgs, ... }@inputs:{
+  outputs = {self, nixpkgs, home-manager, stylix, ... }@inputs:{
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; system = "x86_64-linux"; };
       modules = [
+        stylix.nixosModules.stylix
+        home-manager.nixosModules.home-manager
         ./configuration.nix
-        inputs.home-manager.nixosModules.default
       ];
     };
+
+    # homeConfigurations.blackgolyb = home-manager.lib.homeManagerConfiguration {
+    #   pkgs = nixpkgs.legacyPackages.x86_64-linux // {
+    #     config = { allowUnfree = true; };
+    #   };
+    #   modules = [
+    #     ./home.nix
+    #   ];
+    # };
   };
 }
