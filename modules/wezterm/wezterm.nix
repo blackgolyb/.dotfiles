527adc0e6ae3c9ec988022fd8f5304c4e75b6641
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-16 22:36:04 +0200

    feat(wezterm): fix config file

--- modules/wezterm/wezterm.nix
+++ modules/wezterm/wezterm.nix
@@ -2,5 +2,6 @@
 {
   programs.wezterm.enable = true;
 
-  xdg.configFile."wezterm".source = ./.;
+  xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
+  xdg.configFile."wezterm/colors/custom.toml".source = ./colors/custom.toml;
 }
