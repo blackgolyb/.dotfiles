3cc4bb1e9bf0f8d1b19e62a301ee744b67bf9e7c
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-17 14:01:24 +0200

    feat(nvim): add lualine

--- modules/nvim/lua/core/keymaps.lua
+++ modules/nvim/lua/core/keymaps.lua
@@ -3,3 +3,15 @@ vim.g.maplocalleader = "\\"
 
 -- Filetree
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Explorer", silent = true })
+
+-- Statusbar
+local status_visible = true
+local function toggle_status_bar()
+  if status_visible then
+    vim.opt.laststatus = 0
+  else
+    vim.opt.laststatus = 3
+  end
+  status_visible = not status_visible
+end
+vim.keymap.set("n", "<leader>S", toggle_status_bar, { desc = "Toggle Statusline" })
