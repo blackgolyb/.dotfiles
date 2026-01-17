136aad05afb5fc7aed9f2d1dcfbdc1aa65b04ccb
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-17 15:00:08 +0200

    feat(nvim): add some setting and keymaps

--- modules/nvim/lua/core/base.lua
+++ modules/nvim/lua/core/base.lua
@@ -3,3 +3,11 @@ vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.laststatus = 0
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
--- modules/nvim/lua/core/keymaps.lua
+++ modules/nvim/lua/core/keymaps.lua
@@ -1,6 +1,26 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+
+vim.keymap.set("n", "Q", "<nop>")
+
+-- При гортанні сторінок
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- При пошуку (наступне/попереднє співпадіння)
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste over selection" })
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to bottom window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to top window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
+
+
 -- Filetree
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Filetree", silent = true })
 
