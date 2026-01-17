c2850f82129db29bdd6e53122220210ac450bd34
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-17 13:51:14 +0200

    feat(nvim/neotree): improve file navigations with h and l remap

--- modules/nvim/lua/plugins/neotree.lua
+++ modules/nvim/lua/plugins/neotree.lua
@@ -1,27 +1,41 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    lazy = false,
-    opts = {
-          window = {
-            position = "right",
-            width = 30,
-          },
-          filesystem = {
-            follow_current_file = {
-              enabled = true,
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = false,
+        opts = {
+            window = {
+                position = "right",
+                width = 30,
+                mappings = {
+                    ["h"] = "close_node",
+                    ["l"] = function(state)
+                        local node = state.tree:get_node()
+                        if node.type == "directory" then
+                            if not node:is_expanded() then
+                                require("neo-tree.sources.filesystem.commands").open(state)
+                                else
+                                -- Якщо папка вже відкрита, йдемо до першого дитини (вниз)
+                                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+                            end
+                        end
+                    end,
+                }
             },
-            filtered_items = {
-              hide_dotfiles = false,
-              hide_gitignored = false,
+            filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                },
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
             },
-          },
         }
-  }
+    }
 }
