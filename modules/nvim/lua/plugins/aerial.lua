4fce3643f2887ee547ec8092a4738791281839b1
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-18 17:03:50 +0200

    feat(nvim): add aerial

---
+++ modules/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,19 @@
+return {
+  "stevearc/aerial.nvim",
+  dependencies = {
+     "nvim-treesitter/nvim-treesitter",
+     "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+    require("aerial").setup({
+      layout = {
+        min_width = 30,
+      },
+      manage_folds = false,
+    })
+
+    require("telescope").load_extension("aerial")
+
+    vim.keymap.set("n", "<leader>s", "<cmd>Telescope aerial<CR>", { desc = "Search Symbols (Aerial)" })
+  end,
+}
---
+++ modules/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    }
+}
