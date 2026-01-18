56c9d3ebb90e3b66778501caef328e257c13d84e
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-18 23:53:14 +0200

    fix(zen): fix problem with profile switch

--- home.nix
+++ home.nix
@@ -2,141 +2,153 @@
 {
   imports = [
     ./modules/qtile/qtile.nix
-    ./modules/zsh/zsh.nix
-    ./modules/wezterm/wezterm.nix
-    ./modules/nvim/nvim.nix
-    ./modules/zed/zed.nix
-    ./modules/rofi/rofi.nix
-    ./modules/rofi-bluetooth/rofi-bluetooth.nix
-    ./modules/rofi-network-manager/rofi-network-manager.nix
-    inputs.zen-browser.homeModules.twilight
+      ./modules/zsh/zsh.nix
+      ./modules/wezterm/wezterm.nix
+      ./modules/nvim/nvim.nix
+      ./modules/zed/zed.nix
+      ./modules/rofi/rofi.nix
+      ./modules/rofi-bluetooth/rofi-bluetooth.nix
+      ./modules/rofi-network-manager/rofi-network-manager.nix
+      inputs.zen-browser.homeModules.twilight
   ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+# Home Manager needs a bit of information about you and the paths it should
+# manage.
   home.username = "blackgolyb";
   home.homeDirectory = "/home/blackgolyb";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+# This value determines the Home Manager release that your configuration is
+# compatible with. This helps avoid breakage when a new Home Manager release
+# introduces backwards incompatible changes.
+#
+# You should not change this value, even if you update Home Manager. If you do
+# want to update the value, then make sure to first check the Home Manager
+# release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  nixpkgs.config.allowUnfree = true;
+# The home.packages option allows you to install Nix packages into your
+# environment.
+    nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+# # Adds the 'hello' command to your environment. It prints a friendly
+# # "Hello, world!" when run.
+# pkgs.hello
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+# # It is sometimes useful to fine-tune packages, for example, by applying
+# # overrides. You can do that directly here, just don't forget the
+# # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+# # fonts?
+# (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+# # You can also create simple shell scripts directly inside your
+# # configuration. For example, this adds a command 'my-hello' to your
+# # environment:
+# (pkgs.writeShellScriptBin "my-hello" ''
+#   echo "Hello, ${config.home.username}!"
+# '')
 
-    # CLI
+# CLI
     pass
-    pinentry-curses # gnupg
-    gnupg
-    bat
-    zoxide
-    eza
-    fzf
-    ripgrep
-    brightnessctl # qtile
-    pamixer # qtile
-    uv
-    cloc
-    docker
-    duf
+      pinentry-curses # gnupg
+      gnupg
+      bat
+      zoxide
+      eza
+      fzf
+      ripgrep
+      brightnessctl # qtile
+      pamixer # qtile
+      uv
+      cloc
+      docker
+      duf
 
-    # TUI
-    yazi
-    zellij
-    lazygit
-    lazydocker
-    btop
-    aider-chat
-    wezterm
+# TUI
+      yazi
+      zellij
+      lazygit
+      lazydocker
+      btop
+      aider-chat
+      wezterm
 
-    # Programs
-    xcolor
-    xfce.thunar
-    telegram-desktop
-    pavucontrol # qtile
-    zathura
-    anki
-    chromium
-    logseq
-    super-productivity
-    firefox-devedition
-    mongodb-compass
-    ytmdesktop
-    onlyoffice-desktopeditors
-    vokoscreen-ng
-    vlc
-    flameshot
-    baobab
-    krita
+# Programs
+      xcolor
+      xfce.thunar
+      telegram-desktop
+      pavucontrol # qtile
+      zathura
+      anki
+      chromium
+      logseq
+      super-productivity
+      firefox-devedition
+      mongodb-compass
+      ytmdesktop
+      onlyoffice-desktopeditors
+      vokoscreen-ng
+      vlc
+      flameshot
+      baobab
+      krita
 
-    # Games
-    heroic
-    prismlauncher
-  ];
+# Games
+      heroic
+      prismlauncher
+      ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+# Home Manager is pretty good at managing dotfiles. The primary way to manage
+# plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+# # Building this configuration will create a copy of 'dotfiles/screenrc' in
+# # the Nix store. Activating the configuration will then make '~/.screenrc' a
+# # symlink to the Nix store copy.
+# ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+# # You can also set the file content immediately.
+# ".gradle/gradle.properties".text = ''
+#   org.gradle.console=verbose
+#   org.gradle.daemon.idletimeout=3600000
+# '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/blackgolyb/etc/profile.d/hm-session-vars.sh
-  #
+# Home Manager can also manage your environment variables through
+# 'home.sessionVariables'. These will be explicitly sourced when using a
+# shell provided by Home Manager. If you don't want to manage your shell
+# through Home Manager then you have to manually source 'hm-session-vars.sh'
+# located at either
+#
+#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+#
+# or
+#
+#  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+#
+# or
+#
+#  /etc/profiles/per-user/blackgolyb/etc/profile.d/hm-session-vars.sh
+#
   home.sessionVariables = {
-    # EDITOR = "emacs";
+# EDITOR = "emacs";
     DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
+  };
+  home.shellAliases = {
+    zen = "zen -P \"Default (twilight)\"";
+  };
+
+  xdg.desktopEntries = {
+    zen = {
+      name = "Zen Browser";
+      exec = "zen -P \"Default (twilight)\" %u";
+      terminal = false;
+      categories = [ "Network" "WebBrowser" ];
+    };
   };
 
   stylix.targets = {
-	  zen-browser.enable = false;
-	  };
+    zen-browser.enable = false;
+  };
 
-  # Let Home Manager install and manage itself.
+# Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.gpg.enable = true;
 
