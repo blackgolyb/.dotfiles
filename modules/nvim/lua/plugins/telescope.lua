a7f4aaec18d39a7ef393dae382a2455dc93929a6
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-18 17:27:18 +0200

    feat(nvim): add global search

--- modules/nvim/lua/plugins/telescope.lua
+++ modules/nvim/lua/plugins/telescope.lua
@@ -4,5 +4,13 @@ return {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+    },
+    config = function()
+        local builtin = require('telescope.builtin')
+
+        vim.keymap.set('n', 'g/', builtin.live_grep, { desc = "Global search (Live Grep)" })
+        vim.keymap.set('n', 'g*', builtin.grep_string, { desc = "Search word under cursor" })
+
+        require('telescope').setup({ })
+    end
 }
