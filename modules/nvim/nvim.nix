328bed8b724196e64170182f86277aff8aee9668
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-18 18:08:18 +0200

    refactor: remove mason and install lsp via nix

--- modules/nvim/nvim.nix
+++ modules/nvim/nvim.nix
@@ -15,6 +15,26 @@
     python3
     wget
     curl
+
+    # lsp
+    nixd
+    lua-language-server     # lua_ls
+    nodePackages.typescript-language-server # ts_ls
+    nodePackages.vscode-langservers-extracted # html, cssls, jsonls, eslint
+    tailwindcss-language-server
+    emmet-ls
+    pyright
+    ruff
+    clang-tools             # clangd
+    rust-analyzer
+    elixir-ls
+    nodePackages.bash-language-server
+    dockerfile-language-server
+    hadolint
+    taplo                   # toml
+    yaml-language-server
+    typos-lsp
+    marksman
   ];
 
   xdg.configFile."nvim" = {
