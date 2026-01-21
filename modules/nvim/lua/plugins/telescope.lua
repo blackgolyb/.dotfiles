4c3e4db4abeba0f57c2bbcd4ad1408f93c65b1a8
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-21 22:17:29 +0200

    feat(nvim): run telescope only in insert mode

--- modules/nvim/lua/plugins/telescope.lua
+++ modules/nvim/lua/plugins/telescope.lua
@@ -3,15 +3,26 @@ return {
     tag = '0.2.1',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
         local builtin = require('telescope.builtin')
+        local actions = require('telescope.actions')
 
         vim.keymap.set('n', 'g/', builtin.live_grep, { desc = "Global search (Live Grep)" })
         vim.keymap.set('n', 'g*', builtin.grep_string, { desc = "Search word under cursor" })
 
-        require('telescope').setup({ })
+        require('telescope').setup({
+            defaults = {
+                initial_mode = "insert",
+                mappings = {
+                    i = {
+                        ["<esc>"] = actions.close,
+                    },
+                },
+            }
+        })
+
+        require('telescope').load_extension('fzf')
     end
 }
