c6d32f85ed23a4ab47a95283068eac9e3ee38c72
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-22 00:13:10 +0200

    feat(nvim): change layout for fff

--- modules/nvim/lua/plugins/fff.lua
+++ modules/nvim/lua/plugins/fff.lua
@@ -1,29 +1,38 @@
 return {
-    {
-      'dmtrKovalenko/fff.nvim',
-      build = function()
-        -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-        -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
-        require("fff.download").download_or_build_binary()
-      end,
-      -- if you are using nixos
-      -- build = "nix run .#release",
-      opts = { -- (optional)
-        debug = {
-          enabled = true,     -- we expect your collaboration at least during the beta
-          show_scores = false, -- to help us optimize the scoring system, feel free to share your scores!
-        },
-        prompt = '',
+  {
+    'dmtrKovalenko/fff.nvim',
+    build = function()
+      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+      require("fff.download").download_or_build_binary()
+    end,
+    -- if you are using nixos
+    -- build = "nix run .#release",
+    opts = { -- (optional)
+      debug = {
+        enabled = true,     -- we expect your collaboration at least during the beta
+        show_scores = false, -- to help us optimize the scoring system, feel free to share your scores!
       },
-      -- No need to lazy-load with lazy.nvim.
-      -- This plugin initializes itself lazily.
-      lazy = false,
-      keys = {
-        {
-          "<leader>f", -- try it if you didn't it is a banger keybinding for a picker
-          function() require('fff').find_files() end,
-          desc = 'FFFind files',
-        }
-      }
+      prompt = '',
+      title = 'Files',
+      preview = {
+        enabled = false,
+      },
+      layout = {
+        prompt_position = 'top',
+        height = 0.6,
+        width = 0.4,
+      },
     },
+    -- No need to lazy-load with lazy.nvim.
+    -- This plugin initializes itself lazily.
+    lazy = false,
+    keys = {
+      {
+        "<leader>f", -- try it if you didn't it is a banger keybinding for a picker
+        function() require('fff').find_files() end,
+        desc = 'FFFind files',
+      }
+    }
+  },
 }
