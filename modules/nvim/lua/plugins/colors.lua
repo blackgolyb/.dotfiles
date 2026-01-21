9d22f85509c7729cac6e58dcee8ade7910740aa2
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-22 00:09:30 +0200

    fix(nvim): fix formatting for colors

--- modules/nvim/lua/plugins/colors.lua
+++ modules/nvim/lua/plugins/colors.lua
@@ -1,9 +1,9 @@
 return {
 	{
-'brenoprata10/nvim-highlight-colors',
-	opts = {
-		render = 'virtual',
-		virtual_symbol = '',
-	},
+		'brenoprata10/nvim-highlight-colors',
+		opts = {
+			render = 'virtual',
+			virtual_symbol = '',
+		},
 	}
 };
