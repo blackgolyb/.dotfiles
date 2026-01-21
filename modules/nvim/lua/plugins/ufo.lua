5820f883fd71639ddf24e88d7eae8c1373fe39a4
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-21 21:27:45 +0200

    feat(nvim): add ufo

---
+++ modules/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    event = "BufReadPost",
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { "treesitter", "indent" }
+      end,
+    },
+    config = function(_, opts)
+      vim.o.foldcolumn = '1'
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      local ufo = require('ufo')
+
+      ufo.setup(opts)
+
+      vim.keymap.set('n', 'zR', ufo.openAllFolds, { desc = "Open all folds" })
+      vim.keymap.set('n', 'zM', ufo.closeAllFolds, { desc = "Close all folds" })
+      vim.keymap.set('n', 'K', function()
+        local winid = ufo.peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, { desc = "Peek fold / LSP Hover" })
+    end,
+  },
+}
