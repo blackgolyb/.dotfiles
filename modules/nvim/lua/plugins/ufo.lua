d12be7744ae4cdd79177cbd71c8b9c0af2564199
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-21 22:38:45 +0200

    feat(nvim): disable foldcolumn for ufo

--- modules/nvim/lua/plugins/ufo.lua
+++ modules/nvim/lua/plugins/ufo.lua
@@ -9,10 +9,17 @@ return {
       end,
     },
     config = function(_, opts)
-      vim.o.foldcolumn = '1'
+      vim.o.foldcolumn = '0'
       vim.o.foldlevel = 99
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
+
+      vim.opt.fillchars:append({
+        fold = " ",
+        foldopen = " ",
+        foldsep = " ",
+        foldclose = " ",
+      })
 
       local ufo = require('ufo')
 
