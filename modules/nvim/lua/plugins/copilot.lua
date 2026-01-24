71e4f1c7a9925b0866b11d5395bf5238aa43dd24
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-24 23:21:48 +0200

    feat(nvim): add copilot

---
+++ modules/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        panel = { enabled = false },
+      })
+    end,
+  },
+}
