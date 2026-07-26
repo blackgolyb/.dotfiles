824f9d73d0bfb25c4f581f0761d58a737749cfe0
Blackgolyb <andrejomelnickij@gmail.com>
2026-07-26 16:05:04 +0300

    feat(nvim): add mouse smooth scroll via neoscroll

--- modules/apps/nvim/lua/plugins/ui/neoscroll.lua
+++ modules/apps/nvim/lua/plugins/ui/neoscroll.lua
@@ -8,6 +8,36 @@ local util = require("plugins.util")
 vim.pack.add({ util.gh("karb94/neoscroll.nvim") })
 
 -- setup
-require("neoscroll").setup({
-    duration_multiplier = 0.5,
-})
+local neoscroll = require("neoscroll")
+local config = {
+    neoscroll = {
+        duration_multiplier = 0.5,
+    },
+    mouse = {
+        duration = 50,
+        lines = 3,
+    },
+}
+
+neoscroll.setup(config.neoscroll)
+
+local function mouse_scroll(lines)
+    local winid = vim.fn.getmousepos().winid
+    if winid == 0 or not vim.api.nvim_win_is_valid(winid) then
+        winid = 0
+    end
+
+    neoscroll.scroll(lines, {
+        duration = config.mouse.duration,
+        move_cursor = false,
+        winid = winid,
+    })
+end
+
+vim.keymap.set({ "n", "x", "i" }, "<ScrollWheelUp>", function()
+    mouse_scroll(-config.mouse.lines)
+end, { desc = "Smooth scroll up" })
+
+vim.keymap.set({ "n", "x", "i" }, "<ScrollWheelDown>", function()
+    mouse_scroll(config.mouse.lines)
+end, { desc = "Smooth scroll down" })
